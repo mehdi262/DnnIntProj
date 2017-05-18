0493a5d7c25a41803abf27d297df0127b8ea36c1
M Nikkhah <m.n@gmail.com>
2017-05-18 12:33:37 -0700

    Add project files.

---
+++ DnnInterProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DnnInterProj.Startup))]
+namespace DnnInterProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
