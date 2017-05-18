3256e07f8319189eae175217ba9896cb0024e294
M Nikkhah <m.n@gmail.com>
2017-05-18 13:55:05 -0700

    Add DnnInterProj.DataAccess class library as DAL. in this Commite two Repository class is added inorder to implement the Repository Pattern. IRepository.cs IPersonRepository.cs

---
+++ DnnInterProj.DataAccess/Interface/IPersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DnnInterProj.EntityModel.Model;
+
+
+namespace DnnInterProj.DataAccess
+
+{
+
+    /// <summary>
+    ///  The IPersonRepository interface aims to interoduce the basic functions for CRUD 
+    ///  This interface inherite from the generic IRepository interface 
+    ///  to add work with Person type object and add two new method to it.
+    ///  </summary>
+    ///
+
+    public interface IPersonRepository : IRepository<Person>
+
+    {
+
+        /// <summary>
+        /// Repersent the signiture of the function which retrieves all persons with the similar name to the given name
+        /// </summary>
+        /// <param name="name"> a string, that contains the name of the customer to be searched</param>
+        /// <returns>The return is a List of all persons with the similar Name to the given name as parameter or null if not exist </returns>
+        List<Person> GetByNameLike(string name);
+
+        /// <summary>
+        /// Repersent the signiture of the function which  retrieves The person with the exact username to the given username
+        /// </summary>
+        /// <param name="userName">a string, that contains the username of the customer to be searched</param>
+        /// <returns>a person type object, with the exact username as the given username or null if not exist</returns>
+        Person GetByUserName(string userName);
+
+        /// <summary>
+        /// Repersent the signiture of the function indicate that exact given username is exist in repository
+        /// </summary>
+        /// <param name="userName">a string, that contains the username of the customer to be searched</param>
+        /// <returns>Boolean, True if the given username exist in the repository or False if not</returns>
+        bool CheckUserNameExistance(string userName);
+    }
+}
---
+++ DnnInterProj.DataAccess/Interface/IRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DnnInterProj.EntityModel.Model;
+
+namespace DnnInterProj.DataAccess
+{
+    /// <summary>
+    ///  The generic IRepository interface. 
+    ///  The IRepository interface will be used to introduce the basic CRUD repositories methods
+    /// </summary>
+    /// <typeparam name="T"> generic type of T</typeparam>
+    public interface IRepository<T> where T : EntityBase
+
+    {
+        /// <summary>
+        /// Repersent the signiture of the function which return all existing instances in Repository
+        /// </summary>
+        /// <returns>The return is a List of all instancesor null if not exist any</returns>
+        List<T> GetAll();
+
+        /// <summary>
+        /// Repersent the signiture of the function which return a specific instance with the givin id
+        /// </summary>
+        /// <param name="id"> a long type named id, contains the id of a specific object to be retrieved </param>
+        /// <returns>a T type object, with the equal id as the given id or null if not exist</returns>
+        T GetById(Int64? id);
+
+        /// <summary>
+        /// Repersent the signiture of the function which Create a t type entity and connect to repository  to insert it to the existing repository
+        /// </summary>        
+        /// <param name="entity"> a T type object, which is created and inserted into repository or null if it could not act</param>
+        /// <returns>an integer as the execution status code </returns>
+        int Create(T entity);
+
+        /// <summary>
+        /// Repersent the signiture of the function which  delete the specified object with the given id from the repository
+        /// </summary>
+        /// <param name="entity">a long type named id, contains the id of a specific object to be deleted</param>
+        /// <returns>an integer as the execution status code</returns>
+        int Delete(Int64 entity);
+
+        /// <summary>
+        /// Repersent the signiture of the function which Update an existing object in repository.
+        /// This method  only updates Name, LastName ,Email, and picture 
+        /// of exisiting user based on given id
+        /// the UserName and Joined date cannot be update
+        /// </summary>
+        /// <param name="id">a long type named id, contains the id of a specific object to be deleted</param>
+        /// <param name="person">an object that used its field during the update data</param>
+        /// <returns>an integer as the execution status code </returns>
+        int Update(Int64 id, T entity);
+    }
+}
