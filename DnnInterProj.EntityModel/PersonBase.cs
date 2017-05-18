1bcf10031bb2c60c0aa000c7b349705ed4ce03de
M Nikkhah <m.n@gmail.com>
2017-05-18 13:06:41 -0700

    Add PersonBase class to EntityModel

---
+++ DnnInterProj.EntityModel/PersonBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DnnInterProj.EntityModel.Model
+{
+    /// <summary>
+    /// The basic class of the person.
+    /// this class drived form EntityBase class
+    /// <see cref="EntityBase"/>
+    /// </summary>
+    public class PersonBase : EntityBase
+    {
+        /// <summary>
+        /// Username Field of type string. This the requierd field
+        /// Max lenght 50 caharcter
+        /// </summary>
+        [Required(ErrorMessage = "UserName is Required")]
+        [Display(Name = "User Name")]
+        [StringLength(50)]
+        //[Remote("UserAlreadyExistsAsync", "Account", ErrorMessage = "User with this Email already exists")]
+
+        public string UserName { get; set; }
+    }
+}
